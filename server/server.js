e1699993724d96ca7be119aba601ed2c05be06e3
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-01-17 14:27:49 +0500

    Plugged in all the middleware

---
+++ server/server.js
@@ -0,0 +1,24 @@
+//Dependencies
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+//Routers
+const projectRouter = require("./projectRoutes");
+const actionRouter = require("./actionRoutes");
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+server.use("/api/projects", projectRouter);
+server.use("/api/actions", actionRouter);
+
+function errorHandler(error, req, res, next) {
+  res.status(500).json(error);
+}
+
+server.use(errorHandler);
+
+module.exports = server;
