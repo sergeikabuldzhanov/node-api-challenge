8d4420ff6812233df8f2e06181b81fad782ca061
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-01-17 14:27:08 +0500

    Setup enviroment variables and server listen

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+require("dotenv").config();
+const server = require("./server/server");
+
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
